c55958901125003776ab920f437f8d4579f27730
jesus <jesus@DESKTOP-7CFLNNP>
2020-05-28 13:04:21 -0500

    //SE CREÓ LA CLASE EJEMPLO CON EL MÉTODO MAIN

---
+++ TestProjectJava/src/org/ejemplo/test/ejemplo.java
@@ -0,0 +1,12 @@
+package org.ejemplo.test;
+
+import java.lang.reflect.GenericArrayType;
+
+public class ejemplo {
+
+	public static void main(String[] args) {
+		System.out.println("GIT HUB");
+
+	}
+
+}
